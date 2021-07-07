e25f48cd4ade71edaf0c1781e3e0f6f4184d513b
Nhi Nguyen <NNguyen@ocjones.com>
2021-07-07 16:38:53 -0700

    completed 11.5.3

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,32 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// create a function to iterate through the array of objects in our data file 
-// and then append them to a table row
+// create a function to loop through each object in the array,
+// append a row to the HTML table, and
+// add each value from the object into a cell
 function buildTable(data) {
+      // First, clear out any existing data
     tbody.html("");
-
+    // iterate through each row of the data through the array
     data.forEach((dataRow) => {
+        // create a variable that will append a row to the table body
         let row = tbody.append("tr");
+        // for each object from the array of UFO sightings,
+        // add its values into its own dataRow
+        Object.values(dataRow).forEach((val) => {
+            // create a variable to append data to a table
+            let cell = row.append("td");
+            // extracting only the text of the value,
+            // and add it to the cell
+            cell.text(val);
+            }
+        );
 
     });
+}
+
+// create a function to handle clicks
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
 }
