34129428c27172a18a067c1bcef08bc60b1cfbc6
Nhi Nguyen <NNguyen@ocjones.com>
2021-07-08 16:39:02 -0700

    complete online lessons

--- static/js/app.js
+++ static/js/app.js
@@ -26,10 +26,30 @@ function buildTable(data) {
         );
 
     });
-}
+};
 
 // create a function to handle clicks
 function handleClick() {
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
-}
+
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+// tell D3 to execute our handleClick() function when 
+// the button with an id of filter-btn is clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
